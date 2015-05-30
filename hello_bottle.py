abed09aa17a4f005718432fe535b1eee31797ea0
fargo <peterhjr@gmail.com>
2015-05-30 10:56:52 +0200

    Improved hello bottle

--- hello_bottle.py
+++ hello_bottle.py
@@ -1,7 +1,20 @@
-from bottle import route, run, template
+from bottle import route, run, template, debug
+
+@route('/')
+def home_page():
+    return "Hello Wordl."
+
+@route('/test')
+def test_page():
+    return "This is a test page."
+
+@route('/bye/<name>')
+def index(name):
+    return template("Bye <b>{{name}}</b>!", name=name)
 
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
+debug(True)
 run(host='localhost', port=8080)
